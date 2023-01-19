7ae0a9d3f0eb7487365b9ebadeb847efbe1beec7
lfierz <leonie.fierz@sensirion.com>
2023-01-19 16:04:44 +0100

    Fix comment for i2cBus parameter

--- src/SensirionI2CSfm3000.h
+++ src/SensirionI2CSfm3000.h
@@ -52,7 +52,7 @@ class SensirionI2CSfm3000 {
     /**
      * begin() - Initializes the SensirionI2CSfm3000 class.
      *
-     * @param serial Arduino stream object to be communicated with.
+     * @param i2cBus Arduino stream object to use for communication.
      * @param i2cAddress address of your sensor, depends on SFM3000
      *
      */
